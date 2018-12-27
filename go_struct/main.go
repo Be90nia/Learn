1d5c8efa5843c03782a0d61a48b46093ebe6162c
threedai.lzx <lizx@3daitech.cn>
2018-12-27 17:25:37 +0800

    添加了结构体的学习案例,以及for if switch的学习案例

--- go_struct/main.go
+++ go_struct/main.go
@@ -7,10 +7,57 @@ type person struct {
 	age       int
 }
 
+type student struct {
+	person
+	mail string
+	qq   int
+}
+
+type persons struct {
+	prev      *persons
+	name, sex string
+	age       int
+	next      *persons
+}
+
 func main() {
+	//初始化结构体
 	var people person
 	people.name = "name"
 	people.sex = "male"
 	people.age = 1
 	fmt.Println(people)
+
+	//指针声明结构体
+	var user *person //这样声明指针结构体是不是初始化结构体,因此为nil
+	fmt.Printf("%#v\n", user)
+
+	user1 := new(person)
+	fmt.Printf("%#v\n", user1)  //使用new进行声明结构体,new会初始化结构体
+	fmt.Printf("%#v\n", *user1) //指针指向的数据值
+	fmt.Printf("%#v\n", &user1) //引用指向的指针值
+
+	//结构体指针初始化必须要通过new 或者 &的方式进行初始化
+	var user2 *person = &person{"", "", 1}
+	fmt.Println(user2)
+
+	//一次性初始化
+	//变量名 = 结构体名{成员值1,成员值2}
+	//变量名 = 结构体名{成员名:成员值1,成员名:成员值2}
+
+	//嵌套结构体
+
+	user3 := new(person)
+	user3.name = "begonia"
+	user3.sex = "male"
+	user3.age = 1
+	stu := student{*user3, "begoniali@vip.qq.com", 12345678645}
+	fmt.Printf("%#v\n", stu)
+
+	//结构体指针
+	var stu1, stu2, stu3 persons
+	stu1 = persons{nil, "张三", "男", 32, &stu2}
+	stu2 = persons{&stu1, "李四", "男", 31, &stu3}
+	stu3 = persons{&stu2, "王五", "男", 33, nil}
+	fmt.Printf("%#v\n", stu2)
 }
---
+++ go_switch_for_if/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+	//if条件判断语句
+	//if 条件语句 {
+	//	代码块
+	//}else if 条件语句{
+	// 	代码块
+	//}else{
+	//	代码块
+	//}
+	//条件语句均可以使用 &&、||、!逻辑运算符
+
+	//switch 第一种用法
+	i := 1
+	switch i {
+	case 1:
+		fmt.Printf("i = %#v\n", i)
+	case 2:
+		fmt.Printf("i = %#v\n", i)
+	}
+	//switch 第二种用法
+	i = 3
+	switch {
+	case i < 2:
+		fmt.Printf("i < 2\n")
+	case i < 4:
+		fmt.Printf("i < 4\n")
+	case i < 6:
+		fmt.Printf("i < 6\n")
+	}
+
+	//go语言 只有一个for循环,没有while和do-while,for还有forech功能
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	//如果for后面什么都不写，这样是默认为while(ture)的死循环的
+	i = 0
+	for {
+		if i > 5 {
+			break //跳出循环
+		}
+		i++
+		if i == 3 {
+			continue //跳出当前循环
+		}
+		fmt.Println(i)
+	}
+
+	array := [6]int{1, 2, 3, 4, 5, 6}
+	for i, val := range array {
+		fmt.Printf("%d: %d\n", i, val)
+	}
+
+	newmap := map[string]int{"one": 1, "two": 2}
+	for i, val := range newmap {
+		fmt.Printf("%s: %d\n", i, val)
+	}
+
+}
