544cb0519e36733db2db6796e25c7140efe6296f
Begonia <begoniali@vip.qq.com>
2019-06-27 16:04:18 +0800

    完善其中的文件浏览的报错处理,添加了recover处理panic,以及创建用户可以查看的报错信息

--- go_defer/filelistingserver/filelisting/handler.go
+++ go_defer/filelistingserver/filelisting/handler.go
@@ -4,22 +4,40 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const prefix  = "/list/"
+//创建用户可以查看的报错信息
+type userError string
+//实现其userError报错函数
+func (e userError) Error() string {
+	return e.Message()
+}
+//实现其userError的信息返回函数
+func (e userError) Message() string  {
+	return string(e)
+}
 func HandleFileList(writer http.ResponseWriter,
-	request *http.Request) {
-	path := request.URL.Path[len("/list/"):]
+	request *http.Request) error {
+	//检查URL是否与定义的URL路径一致
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return userError("path must start with " + prefix)
+	}
+	//获取文件路径
+	path := request.URL.Path[len(prefix):]
+	//打开文件
 	file, err := os.Open(path)
 	if err != nil {
-		http.Error(writer,
-			err.Error(),
-			http.StatusInternalServerError)
-		return
+		return err
 	}
 	defer file.Close()
+	//取得文件所有内容
 	all, err := ioutil.ReadAll(file)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	//写入到http页面之上
 	writer.Write(all)
+	return nil
 }
--- go_defer/filelistingserver/web.go
+++ go_defer/filelistingserver/web.go
@@ -6,14 +6,37 @@ import (
 	"net/http"
 	"os"
 )
-
+//创建一个错误处理函数
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
-//
-func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
+//创建用户可以查看的报错信息接口
+type userError interface {
+	error
+	Message() string
+}
+//统一的错误处理
+func errWrapper(
+	handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			//使用recover进行 panic 的错误信息进行处理
+			if r := recover(); r != nil{
+				log.Printf("Painc: %v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
+
 		err := handler(writer, request)
+
 		if err != nil {
 			log.Printf("Error handling request: %s", err.Error())
+			//如果错误信息是userError类型的,则返回错误代码给用户查看
+			//接口的类型会自动识别类型
+			if userErr ,ok := err.(userError); ok{
+				http.Error(writer,userErr.Message(),http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -29,8 +52,8 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/list/",
-		filelisting.HandleFileList)
+	http.HandleFunc("/",
+		errWrapper(filelisting.HandleFileList))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
